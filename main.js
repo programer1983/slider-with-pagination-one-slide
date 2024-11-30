67b49e8c930686d8f30eea27d06516aef0e6bde0
unknown <programer972@gmail.com>
2024-11-30 19:12:00 +0200

    writing logic

---
+++ main.js
@@ -0,0 +1,75 @@
+let slideImages = document.querySelectorAll('img')
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
+let dots = document.querySelectorAll('.dot')
+
+
+var counter = 0
+
+next.addEventListener('click', slideNext)
+
+    function slideNext() {
+        slideImages[counter].style.animation = 'next1 0.5s ease-in forwards'
+        if (counter >=  slideImages.length - 1){
+            counter = 0
+        }else{
+            counter++
+        }
+        slideImages[counter].style.animation = 'next2 0.5s ease-in forwards'
+        indicators()
+    }
+
+prev.addEventListener('click', slidePrev)
+
+    function slidePrev() {
+        slideImages[counter].style.animation = 'prev1 0.5s ease-in forwards'
+        if (counter == 0){
+            counter =  slideImages.length - 1
+        }else{
+            counter--
+        }
+        slideImages[counter].style.animation = 'prev2 0.5s ease-in forwards'
+        indicators()
+    }
+
+    function autoSliding(){
+        deleteInterval = setInterval(timer, 2000)
+            function timer(){
+                slideNext()
+                indicators()
+            }
+    }
+    autoSliding()
+
+
+    const container = document.querySelector('.slide-container')
+    container.addEventListener('mouseover', function(){
+        clearInterval(deleteInterval)
+    })
+
+    container.addEventListener('mouseout', autoSliding)
+
+
+    function indicators(){
+        for(i = 0; i < dots.length; i++){
+            dots[i].className = dots[i].className.replace(' active', '')
+        }
+        dots[counter].className += ' active'
+    }
+
+    function switchImage(currentImage){
+        currentImage.classList.add('active')
+        var imageId = currentImage.getAttribute('attr')
+        if (imageId > counter){
+            slideImages[counter].style.animation = 'next1 0.5s ease-in forwards'
+            counter = imageId
+            slideImages[counter].style.animation = 'next2 0.5s ease-in forwards'
+        }else if(imageId == counter){
+            return
+        }else{
+            slideImages[counter].style.animation = 'prev1 0.5s ease-in forwards'
+            counter = imageId
+            slideImages[counter].style.animation = 'prev2 0.5s ease-in forwards'
+        }
+        indicators()
+    }
